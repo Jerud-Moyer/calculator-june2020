643f412e0df6ca0708156658c009e0cc2432db1a
Jerud-Moyer <jerudmoyer@gmail.com>
2020-06-25 14:35:34 -0700

    calculated changee

--- app.js
+++ app.js
@@ -25,3 +25,39 @@ boogerButton.addEventListener('click', () => {
     // update the textContent of the span
     boogerSpan.textContent = sum;
 });
+
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+const subResult = document.getElementById('difference');
+
+subButton.addEventListener('click', () => {
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+    const difference = Number(value1) - Number(value2);
+    subResult.textContent = difference;
+});
+
+const multInput1 = document.getElementById('mult-input-1');
+const multInput2 = document.getElementById('mult-input-2');
+const multButton = document.getElementById('mult-button');
+const multResult = document.getElementById('product');
+
+multButton.addEventListener('click', () => {
+    const value1 = multInput1.value;
+    const value2 = multInput2.value;
+    const product = Number(value1) * Number(value2);
+    multResult.textContent = product;
+});
+
+const divInput1 = document.getElementById('div-input-1');
+const divInput2 = document.getElementById('div-input-2');
+const divButton = document.getElementById('div-button');
+const divResult = document.getElementById('quotient');
+console.log(divResult);
+divButton.addEventListener('click', () => {
+    const value1 = divInput1.value;
+    const value2 = divInput2.value;
+    const quotient = Number(value1) / Number(value2);
+    divResult.textContent = quotient;
+});
